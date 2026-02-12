96a3d44f0bd980df51cb0ccfa2b1e0baa97a97bd
JinMori07 <griffith7207@gmail.com>
2026-02-12 13:51:59 +0100

    TaskServices.cs

--- dataModel/Classes/TaskService.cs
+++ dataModel/Classes/TaskService.cs
@@ -5,4 +5,37 @@ class TaskSerivce : ITaskService
     private readonly ITaskRepository _repository;
     private readonly List<TaskItem> _tasks;
 
+    public TaskSerivce(ITaskRepository repository)
+    {
+        _repository = repository;
+        _tasks = _repository.LoadTasks();
+    }
+    public IEnumerable<TaskItem> GetAllTasks() => _tasks;
+
+    public void AddTask(string description)
+    {
+        int newId = _tasks.Count > 0 ? _tasks[_tasks.Count - 1].ID + 1 : 1;
+        var newTask = new TaskItem { ID = newId, Description = description, Completed = false};
+        _tasks.Add(newTask);
+        _repository.SaveTasks(_tasks);
+
+    }
+    public void RemoveTask(int id)
+    {
+        var task = _tasks.Find(t => t.ID == id);
+        if (task != null){
+            _tasks.Remove(task);
+            _repository.SaveTasks(_tasks);
+        }
+    }
+    public void ToggleTaskCompletion(int id)
+    {
+        var task = _tasks.Find(t => t.ID == id);
+        if (task != null)
+        {
+            task.Completed = !task.Completed;
+            _repository.SaveTasks(_tasks);
+        }
+    }
+
 }
