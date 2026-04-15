471c953222da89104faa7232a44b37bc1368386a
Basel-1 <159136975+Basel-1@users.noreply.github.com>
2026-04-15 11:48:40 +0200

    Implement HashTable with chaining (Add, Get working)

---
+++ DataStructures/HashTable.cs
@@ -0,0 +1,91 @@
+public class HashTable<K, V> : IHashTable<K, V>
+{
+    private IMyCollection<Entry>[] buckets;
+    private int Capacity;
+    private int _count;
+    public int Count { get => _count; }
+    public HashTable(int capacity = 10)
+    {
+        Capacity = capacity;
+        buckets = new IMyCollection<Entry>[Capacity];
+
+        for (int i = 0; i < Capacity; i++)
+        {
+            buckets[i] = new MyLinkedList<Entry>();
+        }
+    }
+
+    private int GetIndex(K key)
+    {
+        if (key == null)
+            throw new ArgumentNullException(nameof(key));
+        return Math.Abs(key!.GetHashCode()) % Capacity;
+    }
+
+    public void Add(K key, V value)
+    {
+        int index = GetIndex(key);
+        var bucket = buckets[index];
+
+        var it = bucket.GetIterator();
+
+        while (it.HasNext())
+        {
+            var entry = it.Next();
+
+            if (entry.Key.Equals(key))
+            {
+                entry.Value = value;
+                return;
+            }
+        }
+        bucket.Add(new Entry(key, value));
+        _count++;
+    }
+    public V? Get(K key)
+    {
+        int index = GetIndex(key);
+        var bucket = buckets[index];
+
+        var it = bucket.GetIterator();
+
+        while (it.HasNext())
+        {
+            var entry = it.Next();
+
+            if (entry.Key.Equals(key))
+            {
+                return entry.Value;
+            }
+        }
+        return default;
+    }
+    public void Remove(K key)
+    {
+
+    }
+    public bool ContainsKey(K key)
+    {
+        return true;
+    }
+
+
+
+
+    private sealed class Entry : IEquatable<Entry>
+    {
+        public K Key { get; set; }
+        public V Value { get; set; }
+
+        public Entry(K key, V value)
+        {
+            Key = key;
+            Value = value;
+        }
+        public bool Equals(Entry? other)
+        {
+            if (other == null) return false;
+            return Key!.Equals(other.Key);
+        }
+    }
+}
